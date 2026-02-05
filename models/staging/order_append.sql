c02211f322c1b38e4a9840d988734295110a88d7
sai sri <snowflaketraining48@gmail.com>
2026-02-05 03:38:26 +0000

    commit

--- models/staging/order_append.sql
+++ models/staging/order_append.sql
@@ -1,6 +1,6 @@
 {{ config(
-    materialized="incremental",
-    incremental_strategy="append",
+    materialized='incremental',
+    incremental_strategy='append',
 ) }}
 
 select * from {{source('datafeed_shared_schema','raw_orders')}}  limit 15
