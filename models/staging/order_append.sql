99a48d2e82b9898dccf29a520617dd6344ebff95
sai sri <snowflaketraining48@gmail.com>
2026-02-07 03:14:40 +0000

    commit

--- models/staging/order_append.sql
+++ models/staging/order_append.sql
@@ -1,6 +1,7 @@
 {{ config(
     materialized='incremental',
     incremental_strategy='append',
+    post_hook ="delete from {{source('datafeed_shared_schema','raw_orders')}} where id in (select id  from {{source('datafeed_shared_schema','raw_orders')}} limit 15) "
 ) }}
 
 select * from {{source('datafeed_shared_schema','raw_orders')}}  limit 15
